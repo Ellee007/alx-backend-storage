739c0fbaa2e3a687223739fde8676ed6e16fa657
Ellee007 <alhassanaliyu83@gmail.com>
2024-06-19 09:52:50 -0700

    file 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,53 +1,74 @@
 #!/usr/bin/env python3
-"""
-This is our module
-"""
+""" Writing strings to redis """
 import redis
 import uuid
-from typing import Union
-"""
-This is redis module
-This is uuid module
-"""
+import sys
+from functools import wraps
+from typing import Union, Callable, Optional
+
+Types = Union[str, bytes, int, float]
+
+
+def count_calls(method: Callable) -> Callable:
+    """A decorator that counts number of times
+    a method is called"""
+
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """A wrapper"""
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ A decorator to store the history of inputs
+    and outputs for a particular function"""
+
+    input_key = "".join([method.__qualname__, ":inputs"])
+    output_key = "".join([method.__qualname__, ":outputs"])
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """A wrapper"""
+        self._redis.rpush(input_key, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(result))
+        return result
+
+    return wrapper
 
 
 class Cache:
-    """
-    This is cache class
-    """
+    """ Cache class """
 
     def __init__(self):
-        """
-        This is the constructor function
-        """
+        """ constructor """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        store function
-        """
+    @count_calls
+    @call_history
+    def store(self, data: Types) -> str:
+        """ generates a random key ans stores data in redis with the key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key, fn=None):
-        """
-        get function
-        """
-        value = self._redis.get(key)
-        if value is None or fn is None:
-            return value
-        return fn(value)
+    def get(self, key: str, fn: Optional[Callable] = None) -> Types:
+        """Converts data back to desired format using fn"""
+        if fn:
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
 
-    def get_str(self, key):
-        """
-        get_str function
-        """
-        return self.get(key, str)
+    def get_str(self: bytes) -> str:
+        """Parametrize Cache.get with str return"""
+        return self.decode("utf-8")
 
-    def get_int(self, key):
-        """
-        get_int function
-        """
-        return self.get(key, int)
+    def get_int(self: bytes) -> int:
+        """ Parametrize Cache.get to a number """
+        return int.from_bytes(self, sys.byteorder)
